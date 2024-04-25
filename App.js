22cd537ed4693d2b54b3e8d40d4797f00b7ff217
Sasindu Udayanga <90015129+sasinduudayanga@users.noreply.github.com>
2024-04-25 14:44:42 +0530

    Initial commit

---
+++ App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import { Provider } from 'react-native-paper'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+import { theme } from './src/core/theme'
+import StartScreen from './src/screens/StartScreen'
+
+const Stack = createStackNavigator()
+
+export default function App() {
+  return (
+    <Provider theme={theme}>
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="StartScreen"
+          screenOptions={{
+            headerShown: false,
+          }}
+        >
+          <Stack.Screen name="StartScreen" component={StartScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ src/screens/StartScreen.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import Background from '../components/Backgorund'
+import Header from '../components/Header'
+import Button from '../components/Button'
+import Paragraph from '../components/Paragraph'
+
+export default function StartScreen({ navigation }) {
+  return (
+    <Background>
+      <Header>Island Wellness</Header>
+      <Paragraph>
+        The easiest way to start with your amazing application.
+      </Paragraph>
+      <Button
+        mode="contained"
+        onPress={() => navigation.navigate('LoginScreen')}
+      >
+        Login
+      </Button>
+      <Button
+        mode="outlined"
+        // onPress={() => navigation.navigate('RegisterScreen')}
+      >
+        Sign Up
+      </Button>
+    </Background>
+  )
+}
